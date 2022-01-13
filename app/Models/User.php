5ec27742942f5bb48bb084d9c95f7fa43ec98e58
Onur MUTLU <onurmutlu@gmail.com>
2022-01-13 13:20:52 +0300

    BackPack
    Modules

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,16 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Backpack\CRUD\app\Models\Traits\CrudTrait; // <------------------------------- this one
+use Spatie\Permission\Traits\HasRoles;// <---------------------- and this one
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    use CrudTrait; // <----- this
+    use HasRoles; // <------ and this
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
